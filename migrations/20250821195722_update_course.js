9d584ab17921cff6ec41458826617fb44bbde767
Kanishka Maduranga <madurangak@efuturesworld.com>
2025-08-22 01:53:37 +0530

    Create tenat table and add tenant id to rest of tables

---
+++ migrations/20250821195722_update_course.js
@@ -0,0 +1,35 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+    // 1. Add tenant_id column as nullable
+    await knex.schema.alterTable('courses', (table) => {
+      table
+        .integer('tenant_id')
+        .unsigned()
+        .references('id')
+        .inTable('tenants')
+        .onDelete('CASCADE')
+        .nullable();
+    });
+  
+    // 2. Backfill existing users with tenant_id = 1
+    await knex('courses').update({ tenant_id: 1 });
+  
+    // 3. Make column NOT NULL
+    await knex.schema.alterTable('courses', (table) => {
+      table.integer('tenant_id').notNullable().alter();
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = async function (knex) {
+    await knex.schema.alterTable('courses', (table) => {
+      table.dropColumn('tenant_id');
+    });
+  };
+  
